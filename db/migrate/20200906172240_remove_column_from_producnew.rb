d5cf444a4de896e4be9920f7046b4129949e9450
imranrrr <emranlateef786@gmail.com>
2020-12-24 00:21:11 +0500

    Removed unrelated migration

--- db/migrate/20200906172240_remove_column_from_producnew.rb
+++ db/migrate/20200906172240_remove_column_from_producnew.rb
@@ -1,9 +1,9 @@
 class RemoveColumnFromProducnew < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :products, :shipping_price
-  end
+  # def up
+  #   remove_column :products, :shipping_price
+  # end
 
-  def down
-    add_column :products, :shipping_price, :integer
-  end
+  # def down
+  #   add_column :products, :shipping_price, :integer
+  # end
 end
