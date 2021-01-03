60b2af5b2625e01b1fd8ace36a4685b9bdee9394
ajayg <ajayg1567@gmail.com>
2021-01-03 19:25:56 +0530

    nil cant be coerced error fix

--- app/helpers/super_categories_helper.rb
+++ app/helpers/super_categories_helper.rb
@@ -2,6 +2,6 @@ module SuperCategoriesHelper
 	def product_review_rating(product)
 
 		reviews = product.reviews
-		return reviews.pluck(:stars).sum/(reviews.count).to_f
+		return reviews.pluck(:stars).compact.sum/(reviews.count).to_f
 	end
 end
